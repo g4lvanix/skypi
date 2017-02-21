37e19185b1b8ca67126841f31cc690e6ee420d98
g4lvanix <elia@eliaselectronics.com>
2017-02-21 15:39:45 +0100

    something is not quite right here

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 qth = ephem.Observer()
 qth.lat, qth.lon = 51.0209, 13.70844
-qth.pressure = 0
+#qth.pressure = 0
 
 
 with open('stations.txt','r') as f:
@@ -14,4 +14,4 @@ with open('stations.txt','r') as f:
 info = qth.next_pass(zarya)
 print("Rise time: %s Az: %s\n\
 Max alt. time: %s Max alt. Az: %s\n\
-Set time: %s Set Az: %s\n" % info)
+Set time: %s Set Az: %s\n" % (info[0],info[1]*180/np.pi,info[2],info[3]*180/np.pi,info[4],info[5]*180/np.pi) )
