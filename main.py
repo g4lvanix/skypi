7f00ddff6123039d4a77963202c753d67fc9c86c
g4lvanix <elia@eliaselectronics.com>
2017-06-23 17:15:33 +0200

    more debug printing

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def readTLE(fname):
 	sats = []
 	with open(fname,'r') as f:
 		tles = f.readlines()
-		
+
 		for i in range(0,len(tles)-2,3):
 			sats.append(ephem.readtle(tles[i],tles[i+1],tles[i+2]))
 
@@ -41,6 +41,14 @@ def extractWatchlist(sats,watchlist):
 
 	return watched
 
+def getPassInfo(obs,sat):
+	dummy = obs
+	rise_time, rise_az, max_alt_time, max_alt, set_time, set_az = dummy.next_pass(sat)
+	dummy.date = max_alt_time
+	sat.compute(dummy)
+
+	return (rise_time, rise_az, max_alt_time, sat.az, max_alt, set_time, set_az)
+
 
 if __name__ == "__main__":
 	qth = locationSetup(qth_lat,qth_lon,qth_el)
@@ -48,10 +56,9 @@ if __name__ == "__main__":
 	wl = extractWatchlist(sats,qth_watchlist)
 
 	for w in wl:
-		rise_time, rise_az, max_alt_time, max_alt, set_time, set_az = qth.next_pass(w)
+		pi = getPassInfo(qth,w)
 
-		print(w.name,"Rise time: %s  Rise Az: %03.1f\nMax alt. time: %s Max. alt.: %03.1f\nSet time: %s Set Az: %03.1f\n" % (rise_time,np.rad2deg(rise_az),
-		max_alt_time,np.rad2deg(max_alt),set_time,np.rad2deg(set_az)) )
+		print(w.name,"\nRise time: %s  Rise Az: %03.1f\nMax alt. time: %s Max alt. Az: %03.1f Max. El.: %03.1f\nSet time: %s Set Az: %03.1f\n" % (pi[0],np.rad2deg(pi[1]), pi[2],np.rad2deg(pi[3]),np.rad2deg(pi[4]),pi[5],np.rad2deg(pi[6])) )
 
 	#while True:
 
